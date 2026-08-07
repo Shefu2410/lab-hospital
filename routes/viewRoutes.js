51d4382d842087517b9de2571956b5c782af699f
Shefu2410 <shefali9393@.com>
2026-08-07 11:14:35 +0530

    Add signup page route

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const loginPage = require('../views/loginPage');
 const dashboardPage = require('../views/dashboardPage');
 const registrationPage = require('../views/registrationPage');
@@ -9,15 +10,27 @@ const ownerPage = require('../views/ownerPage');
 const router = express.Router();
 
 const send = (page) => (req, res) => {
-  res.set('Content-Type', 'text/html');
-  res.send(page());
+    res.set('Content-Type', 'text/html');
+    res.send(page());
 };
 
+// Login
 router.get(['/', '/index.html'], send(loginPage));
+
+// Dashboard
 router.get('/dashboard.html', send(dashboardPage));
+
+// Registration / Signup
 router.get('/registration.html', send(registrationPage));
+router.get('/signuppage.html', send(registrationPage));
+
+// Test Catalog
 router.get('/catalog.html', send(catalogPage));
+
+// Test Results
 router.get('/results.html', send(resultsPage));
+
+// Owner
 router.get('/owner.html', send(ownerPage));
 
 module.exports = router;
